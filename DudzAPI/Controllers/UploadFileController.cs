f707de13813b146e691a9430a38429e43213de40
meanprogrammer <mean.programmer.d@gmail.com>
2016-03-04 17:55:03 +0800

    11111

--- DudzAPI/Controllers/UploadFileController.cs
+++ DudzAPI/Controllers/UploadFileController.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Hosting;
 using System.Web.Http;
 
@@ -28,9 +30,42 @@ namespace DudzAPI.Controllers
         }
         // POST: api/UploadFile
         [HttpPost, Route("api/file/post")]
-        public void Post()
+        public string Post()
         {
-            
+            int iUploadedCnt = 0;
+
+            // DEFINE THE PATH WHERE WE WANT TO SAVE THE FILES.
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/");
+
+            System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+
+            // CHECK THE FILE COUNT.
+            for (int iCnt = 0; iCnt <= hfc.Count - 1; iCnt++)
+            {
+                System.Web.HttpPostedFile hpf = hfc[iCnt];
+
+                if (hpf.ContentLength > 0)
+                {
+                    // CHECK IF THE SELECTED FILE(S) ALREADY EXISTS IN FOLDER. (AVOID DUPLICATE)
+                    if (!File.Exists(sPath + Path.GetFileName(hpf.FileName)))
+                    {
+                        // SAVE THE FILES IN THE FOLDER.
+                        hpf.SaveAs(sPath + Path.GetFileName(hpf.FileName));
+                        iUploadedCnt = iUploadedCnt + 1;
+                    }
+                }
+            }
+
+            // RETURN A MESSAGE (OPTIONAL).
+            if (iUploadedCnt > 0)
+            {
+                return iUploadedCnt + " Files Uploaded Successfully";
+            }
+            else
+            {
+                return "Upload Failed";
+            }
         }
 
         // PUT: api/UploadFile/5
