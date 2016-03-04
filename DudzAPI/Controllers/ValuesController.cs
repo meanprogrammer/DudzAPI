37f37c0e704be271e29bec1961d319317a9274ae
meanprogrammer <mean.programmer.d@gmail.com>
2016-03-04 10:47:05 +0800

    12342

--- DudzAPI/Controllers/ValuesController.cs
+++ DudzAPI/Controllers/ValuesController.cs
@@ -17,35 +17,35 @@ namespace DudzAPI.Controllers
         [HttpGet, Route("api/values/get")]
         public IHttpActionResult Get()
         {
-            return Ok("Get All");
+            return Ok("GET All");
         }
 
         // GET api/values/5
         [HttpGet, Route("api/values/get/{id}")]
         public IHttpActionResult Get(int id)
         {
-            return Ok(string.Format("Get id={0}", id));
+            return Ok(string.Format("GET id={0}", id));
         }
 
         // POST api/values
         [HttpPost, Route("api/values/post")]
         public IHttpActionResult Post([FromBody]string value)
         {
-            return Ok(value);
+            return Ok(string.Format("POST {0}",value));
         }
 
         // PUT api/values/5
         [HttpPut, Route("api/values/put")]
         public IHttpActionResult Put([FromBody]string value)
         {
-            return Ok(string.Format("value={0}", value));
+            return Ok(string.Format("PUT value={0}", value));
         }
 
         // DELETE api/values/5
         [HttpDelete, Route("api/values/delete")]
         public IHttpActionResult Delete([FromBody]int id)
         {
-            return Ok(string.Format("id={0}", id));
+            return Ok(string.Format("DELETE id={0}", id));
         }
     }
 }
